128fd5815283040e978d554f62f8aea3f3d48c6b
Lalawmpuia Chawngthu <awmach.9@gmail.com>
2023-01-05 14:20:36 +0530

    [add wip]: Pictures model - added base model

---
+++ app/Models/Master/Picture.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models\Master;
+
+use App\Models\BaseModel;
+
+class Picture extends BaseModel
+{
+}
---
+++ database/migrations/2023_01_05_084007_create_pictures_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Master\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('filepath');
+            $table->string('filename');
+            $table->string('mime_type');
+            $table->integer('like_count');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+};
