68334f560805b86f9741d5d24bfb5626e081fd9e
Lalawmpuia Chawngthu <awmach.9@gmail.com>
2023-01-05 16:43:15 +0530

    [update]: web route name change - home changed to login

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Web\Auth;
 use App\Http\Controllers\Web\Master;
 use Illuminate\Support\Facades\Route;
 
-Route::get('', Web\HomeController::class)->name('home');
+Route::get('', Web\HomeController::class)->name('login');
 
 Route::get('register', [Web\AuthController::class, 'register'])->name('register');
 Route::post('sign-up', [Web\AuthController::class, 'sign_up'])->name('submit.sign-up');
