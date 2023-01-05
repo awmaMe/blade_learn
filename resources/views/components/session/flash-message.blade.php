d00c361c71a196a0d0f4667c5508833f7f4e81e5
Lalawmpuia Chawngthu <awmach.9@gmail.com>
2023-01-05 15:33:57 +0530

    [update]: flash message component - removed role attribute

--- resources/views/components/session/flash-message.blade.php
+++ resources/views/components/session/flash-message.blade.php
@@ -1,8 +1,6 @@
 @if ($message)
     <div
-        class="bg-orange-100 border-orange-500 text-orange-700 px-4 py-2 fixed bottom-4 w-1/3 text-center inset-x-0 mx-auto rounded-md"
-        role="alert"
-    >
+        class="bg-orange-100 border-orange-500 text-orange-700 px-4 py-2 fixed bottom-4 w-1/3 text-center inset-x-0 mx-auto rounded-md">
         <p>{{ $message }}</p>
     </div>
 @endif
